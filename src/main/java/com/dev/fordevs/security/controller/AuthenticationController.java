a6517902c0e6a315b5bbf696b7ac26f0cb2f0dd6
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-11-04 18:04:00 +0100

    Security

---
+++ src/main/java/com/dev/fordevs/security/controller/AuthenticationController.java
@@ -0,0 +1,32 @@
+package com.dev.fordevs.security.controller;
+
+import com.dev.fordevs.security.service.AuthenticationService;
+import com.dev.fordevs.security.service.utils.AuthenticationRequest;
+import com.dev.fordevs.security.service.utils.AuthenticationResponse;
+import com.dev.fordevs.security.service.utils.RegisterRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth/")
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authenticationService.register(registerRequest));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest authenticationRequest) {
+        return ResponseEntity.ok(authenticationService.authenticate(authenticationRequest));
+    }
+}
---
+++ src/main/java/com/dev/fordevs/security/service/AuthenticationService.java
@@ -0,0 +1,54 @@
+package com.dev.fordevs.security.service;
+
+import com.dev.fordevs.security.service.utils.AuthenticationRequest;
+import com.dev.fordevs.security.service.utils.AuthenticationResponse;
+import com.dev.fordevs.security.service.utils.RegisterRequest;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(JwtService jwtService, PasswordEncoder passwordEncoder, UserRepository userRepository, AuthenticationManager authenticationManager) {
+        this.jwtService = jwtService;
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public AuthenticationResponse register(RegisterRequest registerRequest) {
+        // TODO: Add User builder
+        var user = User.builder()
+                .build();
+
+        this.userRepository.save(user);
+
+        String accessToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(accessToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest authenticationRequest) {
+        this.authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authenticationRequest.getEmail(),
+                        authenticationRequest.getPassword()
+                )
+        );
+
+        var user = this.userRepository.findUserByName(authenticationRequest.getEmail()).orElseThrow();
+
+        String accessToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(accessToken)
+                .build();
+    }
+}
