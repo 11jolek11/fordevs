668ac25e628b179555a33619a605f9268eeb78db
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-11-04 18:16:32 +0100

    User builder added

--- src/main/java/com/dev/fordevs/security/model/User.java
+++ src/main/java/com/dev/fordevs/security/model/User.java
@@ -179,4 +179,44 @@ public class User implements UserDetails {
                 ", specialization=" + specialization +
                 '}';
     }
+
+    public static class UserBuilder {
+        private String name;
+        private String password;
+        private String email;
+        private Role role;
+        private Specialization specialization;
+
+        public UserBuilder() {
+        }
+
+        public UserBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public UserBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder role(Role role) {
+            this.role = role;
+            return this;
+        }
+
+        public UserBuilder specialization(Specialization specialization) {
+            this.specialization = specialization;
+            return this;
+        }
+
+        public User build() {
+            return new User(this.name, this.password, this.email, this.role, this.specialization);
+        }
+    }
 }
