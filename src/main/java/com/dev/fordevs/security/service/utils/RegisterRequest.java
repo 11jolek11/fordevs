6b8efc793ac2e93c3353f77c5e04ba12549ace24
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-11-04 18:50:39 +0100

    Added missing fields

--- src/main/java/com/dev/fordevs/security/service/utils/RegisterRequest.java
+++ src/main/java/com/dev/fordevs/security/service/utils/RegisterRequest.java
@@ -1,17 +1,24 @@
 package com.dev.fordevs.security.service.utils;
 
 
+import com.dev.fordevs.model.Specialization;
+import com.dev.fordevs.security.model.Role;
+
 import java.util.Objects;
 
 public class RegisterRequest {
     private String name;
     private String password;
     private String email;
+    private Role role;
+    private Specialization specialization;
 
-    public RegisterRequest(String name, String password, String email) {
+    public RegisterRequest(String name, String password, String email, Role role, Specialization specialization) {
         this.name = name;
         this.password = password;
         this.email = email;
+        this.role = role;
+        this.specialization = specialization;
     }
 
     public String getName() {
@@ -38,17 +45,33 @@ public class RegisterRequest {
         this.email = email;
     }
 
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Specialization getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(Specialization specialization) {
+        this.specialization = specialization;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         RegisterRequest that = (RegisterRequest) o;
-        return Objects.equals(name, that.name) && Objects.equals(password, that.password) && Objects.equals(email, that.email);
+        return Objects.equals(name, that.name) && Objects.equals(password, that.password) && Objects.equals(email, that.email) && role == that.role && Objects.equals(specialization, that.specialization);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, password, email);
+        return Objects.hash(name, password, email, role, specialization);
     }
 
     @Override
@@ -57,6 +80,8 @@ public class RegisterRequest {
                 "name='" + name + '\'' +
                 ", password='" + password + '\'' +
                 ", email='" + email + '\'' +
+                ", role=" + role +
+                ", specialization=" + specialization +
                 '}';
     }
 
@@ -64,6 +89,8 @@ public class RegisterRequest {
         private String name;
         private String password;
         private String email;
+        private Role role;
+        private Specialization specialization;
 
         public RegisterRequestBuilder name(String name) {
             this.name = name;
@@ -80,8 +107,18 @@ public class RegisterRequest {
             return this;
         }
 
+        public RegisterRequestBuilder role(Role role) {
+            this.role = role;
+            return this;
+        }
+
+        public RegisterRequestBuilder specialization(Specialization specialization) {
+            this.specialization = specialization;
+            return this;
+        }
+
         public RegisterRequest build() {
-            return new RegisterRequest(this.name, this.password, this.email);
+            return new RegisterRequest(this.name, this.password, this.email, this.role, this.specialization);
         }
 
         @Override
@@ -90,6 +127,8 @@ public class RegisterRequest {
                     "name='" + name + '\'' +
                     ", password='" + password + '\'' +
                     ", email='" + email + '\'' +
+                    ", role=" + role +
+                    ", specialization=" + specialization +
                     '}';
         }
     }
